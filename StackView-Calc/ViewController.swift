454d05148c970d0da0cbed0c9aa9d7e37240390b
prabhanjan-kr <prabhanjankr@protonmail.com>
2018-09-14 23:07:54 +0530

    First Commit, Implemented stack views in Interface builder.

---
+++ StackView-Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView-Calc
+//
+//  Created by prabhanjan on 14/09/18.
+//  Copyright © 2018 manorishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
